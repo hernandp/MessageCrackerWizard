59fa9193410832bde86b6c5c51c57e8bccf44dbc
hzx <2402398917@qq.com>
2024-08-21 01:40:18 +0800

    1. fix 'menu -> view -> window Transparency' menu item cheked bugs
    2. add menu item 'menu -> Edit -> Search listbox... ctrl + s' to search string from listbox [powerful]
    
    3. add menu item 'menu -> Edit -> Clear All Selects' to clear all the selects

--- resource.h
+++ resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by MessageCrackWiz.rc
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MessageCrackWiz.rc 使用
 //
 #define IDD_WIZARD                      101
 #define IDR_MENU1                       102
@@ -74,6 +74,9 @@
 #define ID_EDIT_MESSAGEFILTERS          40024
 #define ID_Menu                         40028
 #define ID_VIEW_DARKCOLORSCHEME         40029
+#define ID_EDIT_                        40032
+#define ID_EDIT_SEARCHLISTBOX           40033
+#define ID_EDIT_CLEARALLSELECTS         40034
 
 // Next default values for new objects
 // 
@@ -81,8 +84,8 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        118
-#define _APS_NEXT_COMMAND_VALUE         40030
-#define _APS_NEXT_CONTROL_VALUE         1046
+#define _APS_NEXT_COMMAND_VALUE         40035
+#define _APS_NEXT_CONTROL_VALUE         1047
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
